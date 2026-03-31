c87b4e94fe930ea2db171b24410b11ebb332d94a
Tejas Kumar <tejaskumar1005@gmail.com>
2026-03-31 12:06:47 +0530

    Data generation python file added and modifies gitIgnore

---
+++ src/data_generation.py
@@ -0,0 +1,46 @@
+import random
+import pandas as pd
+data=[]
+for i in range(1000):
+    product_id=random.randint(1,50)
+    price=random.uniform(10,100)
+    day_of_week=random.randint(0,6)
+    stock_level=random.randint(20,100)
+    days_to_expiry=random.randint(1,10)
+
+    base_demand = 50
+    price_effect = -0.5 * price
+    weekend_boost = 10 if day_of_week in [5,6] else 0
+    expiry_boost = 15 if days_to_expiry < 3 else 0
+
+    units_sold = base_demand + price_effect + weekend_boost + expiry_boost
+
+    noise=random.uniform(-5,5)
+    units_sold+=noise
+    stock_limit = min(stock_level, base_demand)
+
+    units_sold = int(max(0, min(units_sold, stock_limit)))
+
+    data.append([
+        product_id,
+        price,
+        day_of_week,
+        stock_level,
+        days_to_expiry,
+        units_sold
+    ])
+
+df = pd.DataFrame(data, columns=[
+    "product_id",
+    "price",
+    "day_of_week",
+    "stock_level",
+    "days_to_expiry",
+    "units_sold"
+])
+
+print(df.head())
+print(df.describe())
+print(df.corr())
+
+df.to_csv("../data/demand_data.csv", index=False)
